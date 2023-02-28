1f4114cabe3e2d427958f7f007e4115e035ba65f
aparzero30 <jimcin30@gmail.com>
2023-02-28 09:15:00 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PhoneBookComponent } from './book/phone-book/phone-book.component';
+import { AddFormComponent } from './book/add-form/add-form.component';
+import { EditFormComponent } from './book/edit-form/edit-form.component';
+import { FormsModule } from '@angular/forms';
+import { PhoneServiceService } from './phoneservice/phone-service.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PhoneBookComponent,
+    AddFormComponent,
+    EditFormComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [PhoneServiceService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
